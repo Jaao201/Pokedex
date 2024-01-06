fae8a50b85f63d4f9b658591b23282b215830515
Jorge Alysson <jorgealysson201@gmail.com>
2024-01-05 21:25:50 -0300

    add use query pokemon page and pokemon card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,21 @@
+import { PokemonCard } from "../../components/PokemonCard";
+import { useQueryPokemonPage } from "../../hooks/useQuery.PokemonPage";
 import { Container } from "./style";
 
 export function Home() {
+	const { data } = useQueryPokemonPage();
+
+	console.log(data);
+
 	return (
 		<Container>
-			<h1>Home</h1>
+			<h1>{"Bem-vindo(a) à Pokedex do Reprograma Jucás"}</h1>
+
+			<div className="gridCards">
+				{data?.map((pokemon) => {
+					return <PokemonCard pokemon={pokemon} />;
+				})}
+			</div>
 		</Container>
 	);
 }
