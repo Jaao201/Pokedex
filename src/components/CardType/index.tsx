71927d9759a54bfe8d433e51fbea417c4b524d94
Jorge Alysson <jorgealysson201@gmail.com>
2024-01-09 19:18:05 -0300

    add pokemon card and card type!

--- src/components/CardType/index.tsx
+++ src/components/CardType/index.tsx
@@ -2,8 +2,13 @@ import { PokemonType } from "../../@types/pokemon";
 import { Container } from "./style";
 export type TypeProps = {
 	type: PokemonType;
+	size?: number;
 };
 
-export function CardType({ type }: TypeProps) {
-	return <Container type={type}>{type}</Container>;
+export function CardType({ type, size = 10 }: TypeProps) {
+	return (
+		<Container type={type} size={size}>
+			{type}
+		</Container>
+	);
 }
